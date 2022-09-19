dc5941cfd2b3bb2006d9921de930da9b9df410f8
juangmalta <jp_garmal@outlook.com>
2022-09-19 18:05:20 -0500

    inital repo

---
+++ app/src/main/java/com/example/bancodealimentos/ElegirTarjeta_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.bancodealimentos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ElegirTarjeta_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_elegir_tarjeta2)
+    }
+}
---
+++ app/src/main/java/com/example/bancodealimentos/nuevaTarjeta_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.bancodealimentos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class nuevaTarjeta_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nueva_tarjeta)
+    }
+}
